774d33442f72abd98c6e771fa2853c2e058c0c15
ItsHammy <itshammy@protonmail.com>
2025-12-23 21:05:39 +1300

    Formatting changes + Bill count

--- web/index.php
+++ web/index.php
@@ -21,12 +21,10 @@ $count_bills_affected = $db->querySingle('SELECT COUNT(id) FROM bills');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Urgency Statistics - NZPT</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/1.0.0/modern-normalize.min.css">
     <link rel="stylesheet" href="assets/style.css">
     <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="assets/script.js"></script>
     <!-- META TAGS -->
     <meta name="description" content="Free Urgency statistics for the 54th Parliament of New Zealand. Sourced from the Parliament website and made easy to reference.">
@@ -36,20 +34,20 @@ $count_bills_affected = $db->querySingle('SELECT COUNT(id) FROM bills');
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:creator" content="@ohitshammy">
     <meta name="twitter:title" content="Urgency Tracker - New Zealand Politics Tracker">
-    <meta name="twitter:description" content="How many sitting days has the government been in urgency for? See all the stats for free!">
+    <meta name="twitter:description" content="The NZ Govt has passed <?php echo $count_bills_affected; ?> bills under urgency!">
     <meta name="twitter:image" content="https://nzpt.cjs.nz/assets/nzpt-bannertype.png">
     <!-- OPEN GRAPH META -->
     <meta property="og:title" content="How long has the New Zealand Government spend in Urgency?">
     <meta property="og:site_name" content="Urgency Tracker - NZPT">
     <meta property="og:url" content="https://nzpt.cjs.nz/urgency">
-    <meta property="og:description" content="How many sitting days has the government been in urgency for? See all the stats for free!">
+    <meta property="og:description" content="The NZ Govt has passed <?php echo $count_bills_affected; ?> bills under urgency! See all the stats for free!">
     <meta property="og:type" content="website">
     <meta property="og:image" content="https://nzpt.cjs.nz/assets/nzpt-bannertype.png">
 </head>
 <body>
     <header>
         <h1>NZPT | Urgency Tracker</h1>
-        <h5>Another tool by CJ Sandall.</h5>
+        <h5>A tool by CJ Sandall.</h5>
     </header>
     <h2>54th Parliament Statistics:</h2>
     <div class="statistics">
@@ -84,7 +82,7 @@ $count_bills_affected = $db->querySingle('SELECT COUNT(id) FROM bills');
     </div>
     <p><strong>Last Updated:</strong> <span id="last-updated"><?php echo $last_updated; ?></span></p>
     <hr>
-    <section id="explainer-content">
+    <main id="explainer-content">
         <h1>Who are you, why do you care?</h1>
         <p>
         When the New Zealand Parliament goes into urgency, it allows the typical process of lawmaking to be expedited. This means that steps such as public consultation (when the public can give feedback on proposed laws) and select committee review (where a smaller group of MPs scrutinise a bill in detail) can be skipped or shortened.
@@ -107,9 +105,9 @@ $count_bills_affected = $db->querySingle('SELECT COUNT(id) FROM bills');
         </p>
         <hr>
         <h2>The Statistics</h2>
-        <?php echo "<p>The 54th Parliament has officially sat for $num_days_sat days since it was formed on 3rd December 2023. In that time, the coalition government conisiting of the National Party, The ACT Party, and New Zealand First, has put the parliament into Urgency on $num_days_urgent occasions. This makes up $percent_urgency% of all sitting days, with the most recent urgency session being $days_since_urgency days ago on $last_day_urgent_readable. These statistics are scraped from the New Zealand Parliament website, and is updated daily by the NZPT urgency tracker. The urgency tracker system can be seen on Github for those more technically minded. The tracker is a passion project by CJ and will eventually have information about bills affected. The tracker was last updated on $last_updated and last manually checked on December 23rd 2025.</p>";?>
-</section>
-<section id="faqs">
+        <?php echo "<p>The 54th Parliament has officially sat for $num_days_sat days since it was formed on 3rd December 2023. In that time, the coalition government conisiting of the National Party, The ACT Party, and New Zealand First, has put the parliament into Urgency on $num_days_urgency occasions to pass $count_bills_affected bills without the normal amount of consultation and scrutiny. This makes up $percent_urgency% of all sitting days, with the most recent urgency session being $days_since_urgency days ago on $last_day_urgent_readable. These statistics are scraped from the New Zealand Parliament website, and is updated daily by the NZPT urgency tracker. The urgency tracker system can be seen on Github for those more technically minded. The tracker is a passion project by CJ and will eventually have information about bills affected. The tracker was last updated on $last_updated and last manually checked on December 23rd 2025.</p>";?>
+    </main>
+    <section id="faqs">
         <h2>Frequently Asked Questions (FAQs)</h2>
         <h3>What is Urgency?</h3>
         <p>Urgency is a parliamentary procedure that allows the government to expedite the legislative process for certain bills. When a bill is declared urgent, it can bypass some of the usual stages of scrutiny, allowing it to be debated and passed more quickly than under normal circumstances.</p>
