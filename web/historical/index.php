349fd50118fcc8d897f7a129e5329e34c8789194
ItsHammy <itshammy@protonmail.com>
2025-12-25 19:58:58 +1300

    Updated count to be accurate with source

--- web/historical/index.php
+++ web/historical/index.php
@@ -9,7 +9,7 @@ $num_days_urgency_54 = $db->querySingle('SELECT COUNT(id) FROM urgency WHERE in_
 $percent_urgency_54 = $num_days_sat_54 > 0 ? round(($num_days_urgency_54 / $num_days_sat_54) * 100, 2) : 0;
 $last_updated_54 = file_get_contents('../lastupdate.txt');
 $count_bills_affected_54 = $db->querySingle('SELECT COUNT(id) FROM bills');
-$count_total_bills_54 = '170';
+$count_total_bills_54 = '182';
 $ratio_urgent_54 = $count_total_bills_54 > 0 ? round(($count_bills_affected_54 / $count_total_bills_54) * 100, 2) : 0;
 
 // 53rd Parliament Stats
@@ -98,7 +98,7 @@ $PAGE_UPDATED = "December 25th 2025";
                 <li><strong>Total Bills:</strong> <?php echo $count_total_bills_54; ?> <small><a href="https://www.rnz.co.nz/news/on-the-inside/582408/parliament-s-year-in-numbers" target="_blank">Current source.</a></small></li>
                 <li><strong>Ratio of urgent bills:</strong> <?php echo $ratio_urgent_54; ?>%</li>
             </ul>
-            <p class="historical-stats__note"><small>The total bills has been calculated based on available data, however as of December 25th 2025 has not been verified. Please double check before using this as a primary source. (This note will be removed once information has been verified.)</small><p>
+            <p class="historical-stats__note"><small>The total bills has been calculated based on available data, however as of December 25th 2025 has not been verified by NZPT. Please double check before using this as a primary source. (This note will be removed once information has been verified.)</small><p>
         </article>
         
         <article class="stats-card">
